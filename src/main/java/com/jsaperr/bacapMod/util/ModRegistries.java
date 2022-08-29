277d576ebbd177e8dd948c58fe23ad5db2aacd1f
jsaperr <urawizardharry21savage@gmail.com>
2022-08-29 11:35:16 -0700

    added update command w

--- src/main/java/com/jsaperr/bacapMod/util/ModRegistries.java
+++ src/main/java/com/jsaperr/bacapMod/util/ModRegistries.java
@@ -1,5 +1,6 @@
 package com.jsaperr.bacapMod.util;
 
+import com.jsaperr.bacapMod.commands.LinkCommand;
 import com.jsaperr.bacapMod.commands.UpdateCommand;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
@@ -11,6 +12,7 @@ public class ModRegistries {
 
     private static void registerCommands() {
         CommandRegistrationCallback.EVENT.register(UpdateCommand::register);
+        CommandRegistrationCallback.EVENT.register(LinkCommand::register);
     }
 
 }
