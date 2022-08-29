d290bd3b7eec6bd03ab6902386db42a51ef1338f
jsaperr <urawizardharry21savage@gmail.com>
2022-08-29 11:35:10 -0700

    added update command w

--- src/main/java/com/jsaperr/bacapMod/commands/LinkCommand.java
+++ src/main/java/com/jsaperr/bacapMod/commands/LinkCommand.java
@@ -1,44 +1,39 @@
 package com.jsaperr.bacapMod.commands;
 
+import com.github.dhiraj072.randomwordgenerator.RandomWordGenerator;
 import com.github.simplenet.Client;
+import com.github.simplenet.Server;
 import com.github.simplenet.packet.Packet;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.command.CommandRegistryAccess;
-import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.Text;
 
-public class UpdateCommand {
-
+public class LinkCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment)  {
-        dispatcher.register(CommandManager.literal("bacap").then(CommandManager.literal("update"))
+        dispatcher.register(CommandManager.literal("bacap").then(CommandManager.literal("link"))
                 .executes(UpdateCommand::run));
     }
-
     public static int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        Scoreboard scoreboard = MinecraftClient.getInstance().getServer().getScoreboard();
-        String playerName = MinecraftClient.getInstance().player.getName().getString();
-
-        int scoreboardPlayerScore = scoreboard.getPlayerScore(playerName, scoreboard.getObjective("deaths")).getScore();
-
-
         var client = new Client();
+        String playerName = MinecraftClient.getInstance().player.getName().getString();
         client.onConnect(() -> {
             System.out.println(client + " has connected to the server!");
 
             client.postDisconnect(() -> System.out.println(client + " successfully disconnected from the server!"));
         });
-        // Attempt to connect to a server AFTER registering listeners.
-        client.connect("2601:644:0:a010:5876:5cd3:1401:c4cc", 43594);
-        Packet.builder().putByte(2).putInt(scoreboardPlayerScore).queueAndFlush(client);
-        Packet.builder().putByte(1).putString(playerName).queueAndFlush(client);
 
+        String randomWord = RandomWordGenerator.getRandomWord();
+        client.connect("2601:644:0:a010:ceb:3d5:8627:e3ce",43594);
+        Packet.builder().putByte(10).putString(playerName).queueAndFlush();
+        Packet.builder().putByte(11).putString(randomWord).queueAndFlush();
+        System.out.println(randomWord);
 
 
         return 1;
     }
+
 }
